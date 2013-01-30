93271ccdb6b8814947a738245406110f77f5d3df
dashdrum <dan@dashdrum.com>
2013-01-29 21:31:50 -0500

    Added SelectDisabled widget to widgets.py

--- django-snips/widgets.py
+++ django-snips/widgets.py
@@ -6,6 +6,9 @@ by Dan Gentry
 widgets.py
 '''
 
+##====================================================================================##
+
+
 from django.contrib.admin.widgets import RelatedFieldWidgetWrapper
 from django.utils.safestring import mark_safe
 from django.conf import settings
@@ -69,3 +72,78 @@ class CustomRelatedFieldWidgetWrapper(RelatedFieldWidgetWrapper):
             output.append(u'<img src="%simg/admin/icon_addlink.gif" width="10" height="10" alt="%s"/></a>' % (settings.ADMIN_MEDIA_PREFIX, _('Add Another')))
         return mark_safe(u''.join(output))
 
+##====================================================================================##
+
+from django.forms.widgets import Select, force_unicode, flatatt, escape, mark_safe, chain, conditional_escape
+
+class SelectDisabled(Select):
+    
+    """
+        Disables specific items in a select control
+        
+        Example:
+        
+        in forms.py:
+        
+        class MyForm(Form): 
+            def __init__(self, disabled, *args, **kwargs):
+                super(MyForm, self).__init__(*args, **kwargs)
+                if disabled:
+                    self.fields['my_field'].widget.disabled = disabled
+                    
+            my_field = ModelChoiceField(widget=SelectDisabled(),queryset=MyModel.objects.all(),empty_label=None)  
+            
+        in views.py:
+        
+            ## Set a condition in the filter clause of the query to return 
+            ## the disabled items
+        
+            disabled = []
+            for m in MyModel.objects.filter(##condition##).values_list('id'):
+                disabled.append(m[0])
+                
+            form = MyForm( data=request.POST or None, disabled=disabled)
+            
+    """
+    
+    
+    def __init__(self, attrs=None, choices=(), disabled=[]):
+        super(SelectDisabled, self).__init__(attrs, choices)
+        self.disabled = list(disabled)
+
+    def render(self, name, value, attrs=None, choices=()):
+        if value is None: value = ''
+        final_attrs = self.build_attrs(attrs, name=name)
+        output = [u'<select%s>' % flatatt(final_attrs)]
+        options = self.render_options(choices, [value], self.disabled)
+        if options:
+            output.append(options)
+        output.append('</select>')
+        return mark_safe(u'\n'.join(output))
+
+    def render_options(self, choices, selected_choices, disabled_choices):
+        def render_option(option_value, option_label):
+            option_value = force_unicode(option_value)
+            ## This next line adds a mesasge after the label for the option.  Modify as needed ##
+            option_label = (option_value in disabled_choices) and (force_unicode(option_label) + ' - SOLD OUT') or force_unicode(option_label)
+            selected_html = (option_value in selected_choices) and u' selected="selected"' or ''
+            disabled_html = (option_value in disabled_choices) and u' disabled="disabled"' or ''
+            return u'<option value="%s"%s%s>%s</option>' % (
+                escape(option_value), selected_html, disabled_html,
+                conditional_escape(option_label))
+        # Normalize to strings.
+        selected_choices = set([force_unicode(v) for v in selected_choices])
+        disabled_choices = set([force_unicode(v) for v in disabled_choices])
+        output = []
+        for option_value, option_label in chain(self.choices, choices):
+            if isinstance(option_label, (list, tuple)):
+                output.append(u'<optgroup label="%s">' % escape(force_unicode(option_value)))
+                for option in option_label:
+                    output.append(render_option(*option))
+                output.append(u'</optgroup>')
+            else:
+                output.append(render_option(option_value, option_label))
+        return u'\n'.join(output)
+
+
+##====================================================================================##
