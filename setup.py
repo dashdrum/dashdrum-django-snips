0c3eb773403bcc2adcf01c1a374094c2b02f7321
Gentry <dgentry@1404-DAU3124-LT.netgear.com>
2013-03-31 13:47:14 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='dashdrum_django_snips',
+    version='0.1dev',
+    packages=['snips',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
