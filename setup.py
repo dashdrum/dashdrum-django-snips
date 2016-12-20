80a22b133217576a62360dfe5eb3f3991dece470
Dan Gentry <dan@dashdrum.com>
2016-12-20 16:00:07 -0500

    Hoping to include templates in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='dashdrum_django_snips',
     version='0.1dev',
-    packages=['snips','snips/no_password'],
+    packages=find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
