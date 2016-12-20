41d10a61ea22766502a7495d1eb30fb7107f0ffa
Dan Gentry <dan@dashdrum.com>
2016-12-20 15:56:05 -0500

    Update setup.py
    Added no_password as a package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='dashdrum_django_snips',
     version='0.1dev',
-    packages=['snips',],
+    packages=['snips','snips/no_password'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
